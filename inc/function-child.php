2dda9cac93712bf59d853a4d736856f7b11041ca
irawanyulianto <irawanyulianto77@gmail.com>
2025-05-27 09:31:06 +0700

    update

--- inc/function-child.php
+++ inc/function-child.php
@@ -17,24 +17,26 @@ function velocitychild_theme_setup()
 
     if (class_exists('Kirki')) :
 
-        Kirki::add_panel('panel_sekolah', [
+        $text_theme = 'justg';
+
+        Kirki::add_panel('panel_velocity', [
             'priority'    => 10,
-            'title'       => esc_html__('Sekolah', 'justg'),
-            'description' => esc_html__('', 'justg'),
+            'title'       => esc_html__('Velocity Theme', $text_theme),
+            'description' => esc_html__('', $text_theme),
         ]);
         ///Section Color
         Kirki::add_section('section_colorsekolah', [
-            'panel'    => 'panel_sekolah',
-            'title'    => __('Warna', 'justg'),
+            'panel'    => 'panel_velocity',
+            'title'    => __('Warna Tema', $text_theme),
             'priority' => 10,
         ]);
         Kirki::add_field('justg_config', [
             'type'        => 'color',
             'settings'    => 'color_theme',
-            'label'       => __('Warna Tema', 'kirki'),
-            'description' => esc_html__('', 'kirki'),
+            'label'       => __('Warna Tema', $text_theme),
+            'description' => esc_html__('', $text_theme),
             'section'     => 'section_colorsekolah',
-            'default'     => '#345AAA',
+            'default'     => '#00601f',
             'transport'   => 'auto',
             'output'      => [
                 [
@@ -48,20 +50,16 @@ function velocitychild_theme_setup()
                 [
                     'element'   => '.border-color-theme',
                     'property'  => '--bs-border-color',
-                ],
-                [
-                    'element'   => '#primary-menu .dropdown-menu',
-                    'property'  => '--bs-dropdown-link-active-bg',
                 ]
             ],
         ]);
         Kirki::add_field('justg_config', [
             'type'        => 'color',
             'settings'    => 'color_theme2',
-            'label'       => __('Warna Tema 2', 'kirki'),
-            'description' => esc_html__('', 'kirki'),
+            'label'       => __('Warna Tema 2', $text_theme),
+            'description' => esc_html__('', $text_theme),
             'section'     => 'section_colorsekolah',
-            'default'     => '#46a32d',
+            'default'     => '#ffcc36',
             'transport'   => 'auto',
             'output'      => [
                 [
@@ -74,66 +72,181 @@ function velocitychild_theme_setup()
                 ]
             ],
         ]);
-        Kirki::add_field('justg_config', [
-            'type'        => 'background',
-            'settings'    => 'background_themewebsite',
-            'label'       => __('Background Website', 'kirki'),
-            'description' => esc_html__('', 'kirki'),
-            'section'     => 'section_colorsekolah',
-            'default'     => [
-                'background-color'      => 'rgb(235, 235, 235)',
-                'background-image'      => get_stylesheet_directory_uri() . '/images/wall.webp',
-                'background-repeat'     => 'repeat',
-                'background-position'   => 'center center',
-                'background-size'       => 'auto',
-                'background-attachment' => 'fixed',
-            ],
-            'transport'   => 'auto',
-            'output'      => [
-                [
-                    'element'   => ':root[data-bs-theme=light] body',
-                ],
-                [
-                    'element'   => 'body',
-                ],
-            ],
+
+
+        // SECTION: Kontak Header
+        Kirki::add_section('section_header_kontak', [
+            'title'       => esc_html__('Kontak Header', $text_theme),
+            'panel'       => 'panel_velocity',
+            'priority'    => 20,
         ]);
 
-        ///Section Header
-        Kirki::add_section('section_headersekolah', [
-            'panel'    => 'panel_sekolah',
-            'title'    => __('Header', 'justg'),
-            'priority' => 10,
+        // Field: Nomor Telepon
+        Kirki::add_field('justg_config', [
+            'type'        => 'text',
+            'settings'    => 'kontak_telepon',
+            'label'       => esc_html__('Nomor Telepon', $text_theme),
+            'section'     => 'section_header_kontak',
+            'default'     => '',
+            'priority'    => 10,
         ]);
+
+        // Field: Nomor WhatsApp
+        Kirki::add_field('justg_config', [
+            'type'        => 'text',
+            'settings'    => 'kontak_whatsapp',
+            'label'       => esc_html__('Nomor WhatsApp', $text_theme),
+            'section'     => 'section_header_kontak',
+            'default'     => '',
+            'priority'    => 20,
+        ]);
+
+        // Field: Email
+        Kirki::add_field('justg_config', [
+            'type'        => 'text',
+            'settings'    => 'kontak_email',
+            'label'       => esc_html__('Email', $text_theme),
+            'section'     => 'section_header_kontak',
+            'default'     => '',
+            'priority'    => 30,
+        ]);
+
+
+
+        /**
+         * SECTION 1: Banner
+         */
+        Kirki::add_section('section_home_banner', [
+            'panel'    => 'panel_velocity',
+            'title'    => esc_html__('Halaman Depan - Banner', $text_theme),
+            'priority' => 20,
+        ]);
+
         Kirki::add_field('justg_config', [
             'type'        => 'image',
-            'settings'    => 'image_bannerheader',
-            'label'       => esc_html__('Banner Header', 'kirki'),
-            'description' => esc_html__('Upload banner 960x240', 'kirki'),
-            'section'     => 'section_headersekolah',
+            'settings'    => 'home_banner',
+            'label'       => esc_html__('Gambar Banner', $text_theme),
+            'section'     => 'section_home_banner',
             'default'     => '',
-            'partial_refresh'    => [
-                'partial_bannerheader' => [
-                    'selector'        => '.part_bannerheader',
-                    'render_callback' => '__return_false'
-                ]
-            ],
         ]);
-        
-		// section title_tagline
-		Kirki::add_section('title_tagline', [
-			'panel'    => 'panel_sekolah',
-			'title'    => __('Site Identity', $textdomain),
-			'priority' => 10,
-		]);
+
+        /**
+         * SECTION 2: Sambutan 
+         */
+        Kirki::add_section('section_home_sambutan', [
+            'panel'    => 'panel_velocity',
+            'title'    => esc_html__('Halaman Depan - Sambutan', $text_theme),
+            'priority' => 30,
+        ]);
+
+        Kirki::add_field('justg_config', [
+            'type'     => 'image',
+            'settings' => 'home_foto',
+            'label'    => esc_html__('Foto', $text_theme),
+            'section'  => 'section_home_sambutan',
+        ]);
+
+        Kirki::add_field('justg_config', [
+            'type'     => 'text',
+            'settings' => 'home_judul_sambutan',
+            'label'    => esc_html__('Judul Sambutan', $text_theme),
+            'section'  => 'section_home_sambutan',
+        ]);
+
+        Kirki::add_field('justg_config', [
+            'type'     => 'editor',
+            'settings' => 'home_isi_sambutan',
+            'label'    => esc_html__('Isi Sambutan', $text_theme),
+            'section'  => 'section_home_sambutan',
+            'default'  => '',
+        ]);
+
+        /**
+         * SECTION 3: Daftar Guru
+         */
+        Kirki::add_section('section_home_guru', [
+            'panel'    => 'panel_velocity',
+            'title'    => esc_html__('Halaman Depan - Daftar Guru', $text_theme),
+            'priority' => 40,
+        ]);
+
+        Kirki::add_field('justg_config', [
+            'type'     => 'text',
+            'settings' => 'home_judul_guru',
+            'label'    => esc_html__('Judul', $text_theme),
+            'section'  => 'section_home_guru',
+            'default'  => 'Daftar Guru',
+        ]);
+
+        Kirki::add_field('justg_config', [
+            'type'     => 'repeater',
+            'settings' => 'home_guru',
+            'label'    => esc_html__('Daftar Guru', $text_theme),
+            'section'  => 'section_home_guru',
+			'row_label'   => [
+				'type'  => 'text',
+				'value' => __('Guru', 'justg'),
+			],
+            'fields'   => [
+                'foto' => [
+                    'type'  => 'image',
+                    'label' => esc_html__('Foto', $text_theme),
+                ],
+                'nama' => [
+                    'type'  => 'text',
+                    'label' => esc_html__('Nama', $text_theme),
+                ],
+                'jabatan' => [
+                    'type'  => 'text',
+                    'label' => esc_html__('Jabatan', $text_theme),
+                ],
+            ],
+            'default' => [],
+        ]);
+
+        /**
+         * SECTION 4: Galeri Foto
+         */
+        Kirki::add_section('section_home_galeri', [
+            'panel'    => 'panel_velocity',
+            'title'    => esc_html__('Halaman Depan - Galeri Foto', $text_theme),
+            'priority' => 50,
+        ]);
+
+        Kirki::add_field('justg_config', [
+            'type'     => 'text',
+            'settings' => 'home_judul_galeri',
+            'label'    => esc_html__('Judul', $text_theme),
+            'section'  => 'section_home_galeri',
+            'default'  => 'Galeri Foto',
+        ]);
+
+        Kirki::add_field('justg_config', [
+            'type'     => 'repeater',
+            'settings' => 'home_galeri',
+            'label'    => esc_html__('Galeri Foto', $text_theme),
+            'section'  => 'section_home_galeri',
+			'row_label'   => [
+				'type'  => 'text',
+				'value' => __('Foto', 'justg'),
+			],
+            'fields'   => [
+                'gambar' => [
+                    'type'  => 'image',
+                    'label' => esc_html__('Gambar Galeri', $text_theme),
+                ],
+            ],
+            'default' => [],
+        ]);
+
 
         // remove panel in customizer 
         Kirki::remove_panel('global_panel');
-        Kirki::remove_panel('panel_header');
         Kirki::remove_panel('panel_footer');
         Kirki::remove_panel('panel_antispam');
-        Kirki::remove_control('custom_logo');
         Kirki::remove_control('display_header_text');
+        Kirki::remove_section('header_image');
+        Kirki::remove_section('header_section');
 
     endif;
 
@@ -155,14 +268,12 @@ add_action('wp_head', function () {
 if (!function_exists('justg_header_open')) {
     function justg_header_open()
     {
-        echo '<header id="wrapper-header">';
-        echo '<div id="wrapper-navbar" class="container px-2 px-md-0" itemscope itemtype="http://schema.org/WebSite">';
+        echo '<header class="bg-white shadow shadow-sm" id="wrapper-header" itemscope itemtype="http://schema.org/WebSite">';
     }
 }
 if (!function_exists('justg_header_close')) {
     function justg_header_close()
     {
-        echo '</div>';
         echo '</header>';
     }
 }
@@ -179,56 +290,81 @@ function justg_footer_berita()
 {
     require_once(get_stylesheet_directory() . '/inc/part-footer.php');
 }
-add_action('justg_before_wrapper_content', 'justg_before_wrapper_content');
-function justg_before_wrapper_content()
-{
-    echo '<div class="px-2">';
-    echo '<div class="card rounded-0 border-light border-top-0 border-bottom-0 shadow px-0 px-md-2 container">';
-}
-add_action('justg_after_wrapper_content', 'justg_after_wrapper_content');
-function justg_after_wrapper_content()
-{
-    echo '</div>';
-    echo '</div>';
-}
 
 
 //register widget
 add_action('widgets_init', 'justg_widgets_init', 20);
 if (!function_exists('justg_widgets_init')) {
-    function justg_widgets_init()
-    {
-        register_sidebar(
-            array(
-                'name'          => __('Main Sidebar', 'justg'),
-                'id'            => 'main-sidebar',
-                'description'   => __('Main sidebar widget area', 'justg'),
-                'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-                'after_widget'  => '</div></aside>',
-                'before_title'  => '<h3 class="widget-title"><span>',
-                'after_title'   => '</span></h3><div class="p-2">',
-                'show_in_rest'   => false,
-            )
-        );
-    }
+	function justg_widgets_init()
+	{
+		register_sidebar(
+			array(
+				'name'          => __('Main Sidebar', 'justg'),
+				'id'            => 'main-sidebar',
+				'description'   => __('Main sidebar widget area', 'justg'),
+				'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+				'after_widget'  => '</aside>',
+				'before_title'  => '<h6 class="widget-title"><span>',
+				'after_title'   => '</span></h6>',
+				'show_in_rest'   => false,
+			)
+		);
+		// Register footer widget area
+		register_sidebar(
+			array(
+				'name'          => __( 'Footer Widget Area 1', 'justg' ),
+				'id'            => 'footer-widget-1',
+				'description'   => __( '', 'justg' ),
+				'before_widget' => '<aside id="%1$s" class="mb-4 widget %2$s">',
+				'after_widget'  => '</aside>',
+				'before_title'  => '<h6 class="widget-title"><span>',
+				'after_title'   => '</span></h6>',
+			)
+		);
+		register_sidebar(
+			array(
+				'name'          => __( 'Footer Widget Area 2', 'justg' ),
+				'id'            => 'footer-widget-2',
+				'description'   => __( '', 'justg' ),
+				'before_widget' => '<aside id="%1$s" class="mb-4 widget %2$s">',
+				'after_widget'  => '</aside>',
+				'before_title'  => '<h6 class="widget-title"><span>',
+				'after_title'   => '</span></h6>',
+			)
+		);
+		register_sidebar(
+			array(
+				'name'          => __( 'Footer Widget Area 3', 'justg' ),
+				'id'            => 'footer-widget-3',
+				'description'   => __( '', 'justg' ),
+				'before_widget' => '<aside id="%1$s" class="mb-4 widget %2$s">',
+				'after_widget'  => '</aside>',
+				'before_title'  => '<h6 class="widget-title"><span>',
+				'after_title'   => '</span></h6>',
+			)
+		);
+	}
 }
 
-add_action('wp_footer', 'justg_wp_footer');
-function justg_wp_footer()
-{
-    $bgimg = velocitytheme_option('background_themewebsite', '');
-?>
-    <?php if (empty($bgimg) || $bgimg && empty($bgimg['background-image'])) : ?>
-        <style>
-            :root[data-bs-theme=light] body {
-                background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/wall.webp);
-                background-color: #FFF8EB;
-                background-repeat: repeat;
-                background-position: center;
-                background-attachment: fixed;
-                background-size: auto;
-            }
-        </style>
-    <?php endif; ?>
-<?php
+
+if (!function_exists('justg_right_sidebar_check')) {
+	function justg_right_sidebar_check()
+	{
+		if (is_singular('fl-builder-template')) {
+			return;
+		}
+		if (!is_active_sidebar('main-sidebar')) {
+			return;
+		}
+		echo '<div class="right-sidebar widget-area ps-md-2 col-sm-12 col-md-3 order-3" id="right-sidebar" role="complementary">';
+		do_action('justg_before_main_sidebar');
+		dynamic_sidebar('main-sidebar');
+		do_action('justg_after_main_sidebar');
+		echo '</div>';
+	}
 }
+
+function custom_excerpt_length($length) {
+    return 40;
+}
+add_filter('excerpt_length', 'custom_excerpt_length');
