e291f9cc91da36777ae1b0998a46d7b35ff962b8
irawanyulianto <irawanyulianto77@gmail.com>
2025-05-27 09:31:24 +0700

    update

--- inc/part-footer.php
+++ inc/part-footer.php
@@ -1,5 +1,14 @@
-<footer class="site-footer container text-center px-2 px-md-0" id="colophon">
-    <div class="site-info shadow border-light border border-top-0 text-light px-3 py-2" style="background: var(--bs-primary);background-image: var(--bs-gradient);">
+<footer class="site-footer text-center bg-primary text-white pt-2" id="colophon">
+    <div class="row footer-widget container text-start mx-auto py-5">
+        <?php for ($x = 1; $x <= 3; $x++) { ?>
+            <?php if (is_active_sidebar('footer-widget-' . $x)) { ?>
+                <div class="col-md">
+                    <?php dynamic_sidebar('footer-widget-' . $x); ?>
+                </div>
+            <?php } ?>
+        <?php } ?>
+    </div>
+    <div class="site-info bg-dark py-3">
         <small>
             Copyright © <?php echo date("Y"); ?> <?php echo get_bloginfo('name'); ?>. All Rights Reserved.
         </small>
